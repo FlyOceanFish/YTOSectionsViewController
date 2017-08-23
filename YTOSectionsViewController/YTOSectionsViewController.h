92977b82c68d42876f6d78635c8985409c4a4f4d
FlyOceanFish <wangrifei1@126.com>
2017-08-23 11:23:07 +0800

    修改展示不全的bug

--- YTOSectionsViewController/YTOSectionsViewController.h
+++ YTOSectionsViewController/YTOSectionsViewController.h
@@ -6,17 +6,21 @@
 //
 
 #import <UIKit/UIKit.h>
+@class YTOPagesViewController,YTOSegmentControl;
+
 typedef void(^YTOSectionPageChanged)(NSUInteger index);
+
 @interface YTOSectionsViewController : UIViewController
 -(instancetype _Nonnull )initWithTitles:(NSArray<NSString *>* _Nonnull)sectionTitles pageViewControllerClasses:(NSArray<Class> * _Nonnull) pageViewControllerClasses;
+NS_ASSUME_NONNULL_BEGIN
 /**
  需要显示的文本数组
  */
-@property (nonatomic, strong)NSArray<NSString *> * _Nonnull sectionTitles;
+@property (nonatomic, strong)NSArray<NSString *> *sectionTitles;
 /**
  要加载的ViewController，传ClassName即可
  */
-@property(nonatomic, copy)NSArray<Class> * _Nonnull pageViewControllerClasses;
+@property(nonatomic, copy)NSArray<Class> * pageViewControllerClasses;
 
 /**
  默认当前显示第几个
@@ -26,19 +30,37 @@ typedef void(^YTOSectionPageChanged)(NSUInteger index);
 /**
  不选中时文本的颜色
  */
-@property (nonatomic, strong) UIColor * _Nullable titleTextColor;
+@property (nonatomic, strong) UIColor * titleTextColor;
 /**
  文字大小
  */
-@property (nonatomic, strong) UIFont * _Nullable titleTextFont;
+@property (nonatomic, strong) UIFont * titleTextFont;
 
 /**
  选中文本的颜色
  */
-@property (nonatomic, strong) UIColor * _Nullable selectedTitleTextColor;
+@property (nonatomic, strong) UIColor * selectedTitleTextColor;
+/**
+ Indicator的颜色
+ */
+@property (nonatomic, strong) UIColor * selectionIndicatorColor;
 
-@property (nonatomic, copy)YTOSectionPageChanged _Nullable sectonsPageChanged;
+/**
+ 改变视图时的回调
+ */
+@property (nonatomic, copy)YTOSectionPageChanged sectonsPageChanged;
 
+/**
+ 视图容器
+ */
+@property(nonatomic,strong,readonly)YTOPagesViewController * pageVC;
+
+/**
+ 标题容器
+ */
+@property(nonatomic,strong,readonly)YTOSegmentControl * segmentControl;
+
+NS_ASSUME_NONNULL_END
 /**
  设置顶部标题旁边的badge属性
 
@@ -56,4 +78,6 @@ typedef void(^YTOSectionPageChanged)(NSUInteger index);
  @param badgeTextColor badge数字颜色
  */
 - (void)yto_setNumber:(NSInteger)number AtIndex:(NSUInteger)index badgeBgColor:(UIColor *_Nullable)badgeBgColor badgeTextColor:(UIColor *_Nullable)badgeTextColor;
+
+- (void)yto_addParentViewController:(UIViewController *_Nonnull)vc;
 @end
--- YTOSectionsViewController/YTOSectionsViewController.m
+++ YTOSectionsViewController/YTOSectionsViewController.m
@@ -26,12 +26,13 @@
         NSAssert(sectionTitles.count==pageViewControllerClasses.count, @"两个数组大小必须一致");
         self.sectionTitles = sectionTitles;
         self.pageViewControllerClasses = pageViewControllerClasses;
+        [self initDefault];
+        [self initUI];
     }
     return self;
 }
 - (void)viewDidLoad {
     [super viewDidLoad];
-    [self initUI];
 }
 - (void)initDefault{
     self.bagedIndex = -1;
@@ -40,11 +41,20 @@
 - (void)initUI{
     __weak typeof(self) this = self;
     self.segmentControl = [[YTOSegmentControl alloc] initWithFrame:CGRectMake(0, 0, CGRectGetWidth(self.view.bounds), 40)titles:self.sectionTitles segmentWidthStyle:YTOSegmentedControlSegmentWidthStyleFixed];
-    self.segmentControl.titleTextColor = self.titleTextColor;
-    self.segmentControl.selectedTitleTextColor = self.selectedTitleTextColor;
-    self.segmentControl.titleTextFont = self.titleTextFont;
+    if (self.titleTextColor) {
+        self.segmentControl.titleTextColor = self.titleTextColor;
+    }
+    if (self.selectedTitleTextColor) {
+        self.segmentControl.selectedTitleTextColor = self.selectedTitleTextColor;
+    }
+    if (self.titleTextFont) {
+        self.segmentControl.titleTextFont = self.titleTextFont;
+    }
     self.segmentControl.selectionIndicatorLocation = YTOSegmentControlSelectionIndicatorLocationDown;
     self.segmentControl.selectedSegmentIndex = self.defaultPage;
+    if (self.selectionIndicatorColor) {
+        self.segmentControl.selectionIndicatorColor = self.selectionIndicatorColor;
+    }
     [self.segmentControl setIndexChangeBlock:^(NSInteger index){
         this.pageVC.defaultPage = index;
     }];
@@ -52,18 +62,38 @@
         [self yto_setNumber:self.bagedNumber AtIndex:self.bagedIndex badgeBgColor:self.badgeBgColor badgeTextColor:self.badgeTextColor];
     }
     [self.view addSubview:self.segmentControl];
+    
+    _segmentControl.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[view(height)]|" options:0 metrics:@{@"height":@(CGRectGetHeight(self.segmentControl.frame))} views:@{@"view":_segmentControl}]];
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-paddingLeft-[view]-paddingRight-|" options:0 metrics:@{@"paddingLeft":@(-16),@"paddingRight":@(0)} views:@{@"view":_segmentControl}]];
 
     self.pageVC = [[YTOPagesViewController alloc] initWithNavigationOrientation:YTOPageViewControllerNavigationOrientationHorizontal betweenGap:0];
-    self.pageVC.defaultPage = self.defaultPage;
     self.pageVC.pageViewControllerClasses = self.pageViewControllerClasses;
+    self.pageVC.defaultPage = self.defaultPage;
     [self.pageVC setYTOPageViewControllerPageChanged:^(NSUInteger currentPage){
         this.segmentControl.selectedSegmentIndex = currentPage;
     }];
     [self addChildViewController:self.pageVC];
     [self.view addSubview:self.pageVC.view];
-    self.pageVC.view.frame = CGRectMake(0, CGRectGetHeight(self.segmentControl.frame), CGRectGetWidth(self.pageVC.view.frame), CGRectGetHeight(self.pageVC.view.frame));
+    
+    _pageVC.view.translatesAutoresizingMaskIntoConstraints = NO;
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-paddingLeft-[view]-paddingRight-|" options:NSLayoutFormatAlignAllLeft metrics:@{@"paddingLeft":@(-16),@"paddingRight":@"-16"} views:@{@"view":_pageVC.view}]];
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-[view2]-[view]-|" options:NSLayoutFormatAlignAllLeft metrics:nil views:@{@"view":_pageVC.view,@"view2":_segmentControl}]];
+
+//    self.pageVC.view.frame = CGRectMake(0, CGRectGetHeight(self.segmentControl.frame), CGRectGetWidth(self.pageVC.view.frame), CGRectGetHeight(self.pageVC.view.frame));
     [self.pageVC didMoveToParentViewController:self];
 }
+- (void)yto_addParentViewController:(UIViewController *)vc{
+    [vc addChildViewController:self];
+    [vc.view addSubview:self.view];
+    
+    self.view.translatesAutoresizingMaskIntoConstraints = NO;
+    [vc.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-[view]-|" options:NSLayoutFormatAlignAllLeft metrics:nil views:@{@"view":self.view}]];
+    [vc.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-[view]-|" options:NSLayoutFormatAlignAllLeft metrics:nil views:@{@"view":self.view}]];
+    vc.view.layoutMargins = UIEdgeInsetsMake(0, 0, 0, 0);
+    [self didMoveToParentViewController:vc];
+}
+
 -(void)yto_setNumber:(NSInteger)number AtIndex:(NSUInteger)index{
     [self yto_setNumber:number AtIndex:index badgeBgColor:nil badgeTextColor:nil];
 }
@@ -78,7 +108,6 @@
     }
 
 }
-
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
